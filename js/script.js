c3bcecf94ffba14c8618915218770a52d9e7efb0
Ronney Vargas <ronneyvargas@gmail.com>
2019-04-29 22:17:04 -0500

    added js to slow down the scrolling of the page

--- js/script.js
+++ js/script.js
@@ -94,4 +94,13 @@ $(document).ready(function() {
         return false;
     });
    
+    $(".navbar li a").click(function(e) {
+        e.preventDefault();
+
+        var targetElement = $(this).attr("href");
+        var targetPosition = $(targetElement).offset().top;
+
+        $("html, body").animate({scrollTop: targetPosition - 50}, "slow");
+    });
+
 });
