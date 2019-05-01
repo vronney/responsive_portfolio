5aa4a41a8afc44615608e4a92416247506269d06
Ronney Vargas <ronneyvargas@gmail.com>
2019-04-30 19:46:41 -0500

    Moved isotope script to top

--- js/script.js
+++ js/script.js
@@ -2,6 +2,16 @@ $(window).on("load", function() {
     $(".loader .inner").fadeOut(500, function() {
         $(".loader").fadeOut(750);
     });
+
+    $(".items").isotope({
+        filter: "*",
+        animationOptions: {
+            duration: 1500,
+            easing: "linear",
+            queue: false
+        }
+    });
+
 });
 
 $(document).ready(function() {
@@ -72,15 +82,6 @@ $(document).ready(function() {
     });
 
     $("[data-fancybox]").fancybox();
-
-    $(".items").isotope({
-        filter: "*",
-        animationOptions: {
-            duration: 1500,
-            easing: "linear",
-            queue: false
-        }
-    });
 
     $("#filters a").click(function() {
         $("#filters .current").removeClass("current");
